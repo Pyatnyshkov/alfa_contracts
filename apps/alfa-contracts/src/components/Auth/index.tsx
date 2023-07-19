8b9a5eca685287382480ca7d81dc28be172e1c52
Николай Пятнышков <n.piatnyshkov@egopay.ru>
2023-07-19 14:24:16 +0300

    add jwt decode

--- apps/alfa-contracts/src/components/Auth/index.tsx
+++ apps/alfa-contracts/src/components/Auth/index.tsx
@@ -4,15 +4,15 @@ import FormField from 'arui-feather/form-field';
 import Input from 'arui-feather/input';
 import Button from 'arui-feather/button';
 import { GoogleLogin } from '@react-oauth/google';
-import google from '../../assets/google.svg';
+import { setAuth } from '../../store/reducers/app';
+import { useAppDispatch } from '../../hooks/useAppSelector';
 
 import { useLazyAuthQuery } from '../../services/auth';
 
 import styles from './index.module.scss';
 
-const GoogleIcon = () => <img src={google} alt="" className={styles['icon']} />;
-
 const Auth = () => {
+  const dispatch = useAppDispatch();
   const [oauth] = useLazyAuthQuery();
   return (
     <div className={styles['component']}>
@@ -33,17 +33,11 @@ const Auth = () => {
           </Button>
         </FormField>
       </Form>
-      {/*<Button*/}
-      {/*  size="s"*/}
-      {/*  className={styles.google}*/}
-      {/*  onClick={() => oauth(null)}*/}
-      {/*  icon={<GoogleIcon />}*/}
-      {/*>*/}
-      {/*  Вход через аккаунт гугл*/}
-      {/*</Button>*/}
+
       <div className={styles['google']}>
         <GoogleLogin
           onSuccess={(credentialResponse) => {
+            dispatch(setAuth(credentialResponse.credential));
             console.log(credentialResponse);
           }}
           onError={() => {
--- apps/alfa-contracts/src/store/reducers/app.ts
+++ apps/alfa-contracts/src/store/reducers/app.ts
@@ -1,14 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { PayloadAction } from '@reduxjs/toolkit/dist/createAction';
+import { IUser } from '../../models/user';
+
+import * as jose from 'jose'
 
 interface AppState {
   navigation: string;
+  user: IUser
   isAuth: boolean;
 }
 
 const initialState: AppState = {
   navigation: 'contracts',
   isAuth: JSON.parse(localStorage.getItem('isAuth') || 'false'),
+  user: {}
 };
 
 const appSlice = createSlice({
@@ -18,9 +23,15 @@ const appSlice = createSlice({
     setNavigation(state, action: PayloadAction<string>) {
       state.navigation = action.payload;
     },
-    setAuth(state, action: PayloadAction<boolean>) {
-      localStorage.setItem('isAuth', JSON.stringify(action.payload));
-      state.isAuth = action.payload;
+    setAuth(state, action: PayloadAction<string | undefined>) {
+      localStorage.setItem('isAuth', JSON.stringify(!!action.payload));
+      if (typeof action.payload === 'string') {
+        const credentials = jose.decodeJwt(action.payload);
+        state.user = {name: credentials.name as string};
+        state.isAuth = true;
+      } else {
+        state.isAuth = false;
+      }
     },
   },
 });
